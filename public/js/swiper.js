dde7527cffdb1d0dbba2481e647b51be36c554c0
kusumadeveloper01 <idabagustrikusumadewa@gmail.com>
2025-10-08 23:08:57 +0800

    update final

---
+++ public/js/swiper.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".typePsSwiper", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1280: {
+      slidesPerView: 4,
+    },
+  },
+});
