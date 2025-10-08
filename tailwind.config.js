4b037df8cfa7f2784ae8e91947d670c82687d6a5
kusumadeveloper01 <idabagustrikusumadewa@gmail.com>
2025-10-08 12:20:14 +0800

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./public/pages/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        urbanist: "Urbanist",
+      },
+
+      colors: {
+        // general
+        "accent-primary": "#9257F3",
+
+        // background
+        "background-primary": "#171717",
+        "background-secondary": "#1A1A1A",
+
+        // text
+        "text-primary": "#FFFFFFb3",
+        "text-secondary": "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 };
